62de7137217e37ef873385141b21989d00f75203
EduGese <122921699+EduGese@users.noreply.github.com>
2025-01-25 21:10:43 +0100

    feat: initialize Express app with CORS, MongoDB connection, and routes setup

---
+++ favorites-microservice/src/app.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import connectDB from './config/db';
+import config from './config/config';
+import userRoutes from './routes/favorites.routes';
+
+//execute express
+const app = express();
+const port = config.server.port;
+
+//CORS
+var corsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 204,
+  maxAge: 500,
+  origin: config.client.url,
+}
+
+//middlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Mongodb conection
+connectDB();
+
+//routes
+app.use('/api/favorites', userRoutes);
+
+// server listenening on config.server.port
+app.listen(port, () => {
+  console.log('Server is running on port', port);
+  console.log('Environment:', config.env);
+});
+
+// Mongodb conection
+connectDB();
