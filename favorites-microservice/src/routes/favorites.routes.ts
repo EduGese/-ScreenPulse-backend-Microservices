d0183b0b8e07a7e2ce716e65e761703f7763cc09
EduGese <122921699+EduGese@users.noreply.github.com>
2025-01-25 21:14:21 +0100

    feat: implement routes, controllers, and service logic for favorites

---
+++ favorites-microservice/src/routes/favorites.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import favoritesController from "../controllers/favorites.controller";
+
+
+const router = express.Router();
+
+//Create 
+router.post('/favorites/:id', favoritesController.createFavorite);
+
+//Get all
+router.get('/favorites/:id', favoritesController.getFavorites);
+
+//Delete by Id
+router.delete('/favorites/:id/:userId', favoritesController.deleteFavorite);
+
+//Update by Id
+router.put('/favorites/:id/:userId', favoritesController.updateFavorite);
+
+export default router;
