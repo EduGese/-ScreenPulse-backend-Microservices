4ea8a8d4ed3c467937ba3a189e660f7aeeb736ef
EduGese <122921699+EduGese@users.noreply.github.com>
2025-01-26 19:11:09 +0100

    feat: add configuration config.ts in omdb-microservice

---
+++ omdb-microservice/src/config/config.ts
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Detect environment in use
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+
+//Select variables depending on environment
+const MONGODB_URL =
+    NODE_ENV === 'production'
+        ? process.env.PROD_MONGODB_URL
+        : process.env.DEV_MONGODB_URL;
+
+const CLIENT_URL =
+    NODE_ENV === 'production'
+        ? process.env.PROD_CLIENT_URL
+        : process.env.DEV_CLIENT_URL;
+
+//Server Port
+const PORT = process.env.PORT ? Number(process.env.PORT) : 5000;
+
+//centralized config
+const config = {
+    env: NODE_ENV,
+    server: {
+        port: PORT,
+    },
+    mongo: {
+        url: MONGODB_URL || '',
+    },
+    client: {
+        url: CLIENT_URL || '',
+    },
+};
+
+export default config;
